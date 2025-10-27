50275d75ad9459482f14267b775198e9c3e382fe
PapproxNP <DMYTRO.STOPETS@lnu.edu.ua>
2025-10-27 20:44:12 +0200

    BLL/Core: Bitboard stub

---
+++ Bogar.BLL/Core/Bitboard.cs
@@ -0,0 +1,22 @@
+namespace Bogar.BLL.Core;
+
+public static class Bitboard
+{
+    public const ulong EMPTY = 0x0UL;
+    public const ulong UNIVERSE = 0xFFFFFFFFFFFFFFFFUL;
+
+    public static int BitScanForward(ulong bitboard)
+    {
+        return -1;
+    }
+
+    public static int BitScanReverse(ulong bitboard)
+    {
+        return -1;
+    }
+
+    public static int PopCount(ulong bitboard)
+    {
+        return -1;
+    }
+}
