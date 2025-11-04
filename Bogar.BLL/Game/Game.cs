01ac7fc98b24bd1606bdccc6a69f4144d5288337
Nikita Batsamut <nbatsamut@synergist.technology>
2025-11-04 21:20:09 +0200

    Add: game logic and tests for that

---
+++ Bogar.BLL/Game/Game.cs
@@ -0,0 +1,105 @@
+using Bogar.BLL.Core;
+using Bogar.BLL.Player;
+using System;
+using System.Collections.Generic;
+namespace Bogar.BLL.Game;
+public class Game
+{
+    private readonly Player.Player _whitePlayer;
+    private readonly Player.Player _blackPlayer;
+    private readonly Position _position;
+
+    public List<Move> Moves { get; private set; }
+
+    public Game(Player.Player pl1, Player.Player pl2)
+    {
+        _whitePlayer = pl1;
+        _blackPlayer = pl2;
+        _position = new Position();
+        Moves = new List<Move>();
+    }
+
+    public int GetScore()
+    {
+        var score = _position.CalculateScore();
+        return score.whiteScore - score.blackScore;
+    }
+
+    public void DoNextMove()
+    {
+        if (Moves.Count >= 32)
+        {
+            throw new InvalidOperationException("Game is over. Cannot make more moves.");
+        }
+
+        bool isWhiteTurn = (Moves.Count % 2 == 0);
+        Player.Player currentPlayer = isWhiteTurn ? _whitePlayer : _blackPlayer;
+        Color currentSide = isWhiteTurn ? Color.White : Color.Black;
+
+        string moveString = currentPlayer.GetMove(Moves);
+        if (string.IsNullOrEmpty(moveString))
+        {
+            throw new InvalidOperationException($"Bot ({currentSide}) returned a null or empty move.");
+        }
+
+        Move newMove;
+        try
+        {
+            newMove = ParseMoveString(moveString, currentSide);
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"Bot ({currentSide}) returned an invalid move string: '{moveString}'.",
+                ex);
+        }
+
+        _position.DoMove(newMove);
+
+        Moves.Add(newMove);
+    }
+
+    private Move ParseMoveString(string moveString, Color sideToMove)
+    {
+        if (string.IsNullOrEmpty(moveString) || moveString.Length < 2)
+        {
+            throw new FormatException($"Invalid move string format: {moveString}");
+        }
+
+        char pieceChar = moveString[0];
+
+        string squareString = moveString.Substring(1).ToUpper();
+        if (!Enum.TryParse<Square>(squareString, out Square square))
+        {
+            throw new FormatException($"Invalid square in move: {squareString}");
+        }
+
+        Piece piece;
+        if (sideToMove == Color.White)
+        {
+            piece = pieceChar switch
+            {
+                'P' => Piece.WhitePawn,
+                'N' => Piece.WhiteKnight,
+                'B' => Piece.WhiteBishop,
+                'R' => Piece.WhiteRook,
+                'Q' => Piece.WhiteQueen,
+                'K' => Piece.WhiteKing,
+                _ => throw new FormatException($"Invalid piece type in move: {pieceChar}")
+            };
+        }
+        else
+        {
+            piece = pieceChar switch
+            {
+                'P' => Piece.BlackPawn,
+                'N' => Piece.BlackKnight,
+                'B' => Piece.BlackBishop,
+                'R' => Piece.BlackRook,
+                'Q' => Piece.BlackQueen,
+                'K' => Piece.BlackKing,
+                _ => throw new FormatException($"Invalid piece type in move: {pieceChar}")
+            };
+        }
+        return new Move(piece, square);
+    }
+}
