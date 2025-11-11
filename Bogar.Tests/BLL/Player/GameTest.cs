75d6ceffbfb1fc33e450736dc1fd4687fed3e3e0
PapproxNP <DMYTRO.STOPETS@lnu.edu.ua>
2025-11-11 22:56:26 +0200

    hotfix

--- Bogar.Tests/BLL/Player/GameTest.cs
+++ Bogar.Tests/BLL/Player/GameTest.cs
@@ -12,8 +12,8 @@ namespace Bogar.BLL.Game
 {
     public class GameTest
     {
-        private const string WhiteBotPath = @".\random_bot.exe";
-        private const string BlackBotPath = @".\random_bot.exe";
+        private const string WhiteBotPath = @"..\..\..\test_bots\random_bot.exe";
+        private const string BlackBotPath = @"..\..\..\test_bots\random_bot.exe";
 
         private readonly ITestOutputHelper _output;
 
