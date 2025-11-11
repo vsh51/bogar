236c9cda9c6e58ab206c322dfb33147b99b74fe7
cheliz <roikeliz2006@gmail.com>
2025-11-11 23:58:57 +0200

    add start window

---
+++ Bogar.UI/StartWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bogar.UI
+{
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LocalGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void CreateLobbyButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ConnectServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
