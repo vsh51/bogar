977231150287324974abdfc6344932fd902cb4ad
PapproxNP <150626349+PapproxNP@users.noreply.github.com>
2025-11-18 20:40:55 +0200

    UI: Animation implemented

--- Bogar.UI/MainWindow.xaml.cs
+++ Bogar.UI/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using Bogar.UI.Chess;
 using Bogar.BLL.Core;
 using BllColor = Bogar.BLL.Core.Color;
@@ -17,6 +19,8 @@ namespace Bogar.UI
     {
         private Chess.Board chessBoard = new Chess.Board();
         private GameManager gameManager = new GameManager();
+        private readonly Dictionary<(BllColor, BllPieceType), FrameworkElement> _pieceSourceElements = new();
+        private readonly Duration _moveAnimationDuration = new Duration(TimeSpan.FromMilliseconds(450));
 
         private ObservableCollection<string> leftMoves = new ObservableCollection<string>();
         private ObservableCollection<string> rightMoves = new ObservableCollection<string>();
@@ -34,6 +38,7 @@ namespace Bogar.UI
 
             LeftMovesList.ItemsSource = leftMoves;
             RightMovesList.ItemsSource = rightMoves;
+            ConfigurePieceSources();
 
             SetupGameManager();
             GenerateBoard();
@@ -61,6 +66,23 @@ namespace Bogar.UI
             gameManager.TimerTick += OnTimerTick;
         }
 
+        private void ConfigurePieceSources()
+        {
+            _pieceSourceElements[(BllColor.Black, BllPieceType.Pawn)] = BlackPawnSource;
+            _pieceSourceElements[(BllColor.Black, BllPieceType.Knight)] = BlackKnightSource;
+            _pieceSourceElements[(BllColor.Black, BllPieceType.Bishop)] = BlackBishopSource;
+            _pieceSourceElements[(BllColor.Black, BllPieceType.Rook)] = BlackRookSource;
+            _pieceSourceElements[(BllColor.Black, BllPieceType.Queen)] = BlackQueenSource;
+            _pieceSourceElements[(BllColor.Black, BllPieceType.King)] = BlackKingSource;
+
+            _pieceSourceElements[(BllColor.White, BllPieceType.Pawn)] = WhitePawnSource;
+            _pieceSourceElements[(BllColor.White, BllPieceType.Knight)] = WhiteKnightSource;
+            _pieceSourceElements[(BllColor.White, BllPieceType.Bishop)] = WhiteBishopSource;
+            _pieceSourceElements[(BllColor.White, BllPieceType.Rook)] = WhiteRookSource;
+            _pieceSourceElements[(BllColor.White, BllPieceType.Queen)] = WhiteQueenSource;
+            _pieceSourceElements[(BllColor.White, BllPieceType.King)] = WhiteKingSource;
+        }
+
         private void OnTimerTick(TimeSpan elapsed)
         {
             GameTimerText.Text = elapsed.ToString(@"mm\:ss\:ff");
@@ -77,6 +99,7 @@ namespace Bogar.UI
                     targetList.RemoveAt(0);
                 targetList.Add(moveString);
 
+                AnimatePieceArrival(move, playerColor);
                 SyncBoardWithGameState();
                 UpdatePieceCounters();
                 UpdateScoreDisplay();
@@ -97,6 +120,98 @@ namespace Bogar.UI
             {
                 Console.WriteLine($"Game Status: {status}");
             });
+        }
+
+        private void AnimatePieceArrival(BllMove move, BllColor moverColor)
+        {
+            if (AnimationCanvas == null || !_pieceSourceElements.TryGetValue((moverColor, PieceExtensions.TypeOfPiece(move.Piece)), out var sourceElement))
+                return;
+
+            var targetBorder = GetBorderForSquare(move.Square);
+            if (sourceElement == null || targetBorder == null)
+                return;
+
+            var start = GetElementCenterOnCanvas(sourceElement);
+            var end = GetElementCenterOnCanvas(targetBorder);
+            if (!start.HasValue || !end.HasValue)
+                return;
+
+            var resourceKey = GetPieceResourceKey(moverColor, PieceExtensions.TypeOfPiece(move.Piece));
+            if (TryFindResource(resourceKey) is not ImageSource imageSource)
+                return;
+
+            var animatedImage = new Image
+            {
+                Source = imageSource,
+                Width = 40,
+                Height = 40,
+                RenderTransform = new TranslateTransform()
+            };
+
+            AnimationCanvas.Children.Add(animatedImage);
+
+            double startLeft = start.Value.X - animatedImage.Width / 2;
+            double startTop = start.Value.Y - animatedImage.Height / 2;
+            double endLeft = end.Value.X - animatedImage.Width / 2;
+            double endTop = end.Value.Y - animatedImage.Height / 2;
+
+            Canvas.SetLeft(animatedImage, startLeft);
+            Canvas.SetTop(animatedImage, startTop);
+
+            var easing = new CubicEase { EasingMode = EasingMode.EaseInOut };
+            var leftAnimation = new DoubleAnimation(startLeft, endLeft, _moveAnimationDuration) { EasingFunction = easing };
+            var topAnimation = new DoubleAnimation(startTop, endTop, _moveAnimationDuration) { EasingFunction = easing };
+
+            leftAnimation.Completed += (s, e) => AnimationCanvas.Children.Remove(animatedImage);
+
+            animatedImage.BeginAnimation(Canvas.LeftProperty, leftAnimation);
+            animatedImage.BeginAnimation(Canvas.TopProperty, topAnimation);
+        }
+
+        private Point? GetElementCenterOnCanvas(FrameworkElement element)
+        {
+            if (AnimationCanvas == null || !element.IsLoaded)
+                return null;
+
+            try
+            {
+                var transform = element.TransformToVisual(AnimationCanvas);
+                return transform.Transform(new Point(element.ActualWidth / 2, element.ActualHeight / 2));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private Border? GetBorderForSquare(BllSquare square)
+        {
+            int col = square.GetFile();
+            int row = 7 - square.GetRank();
+
+            foreach (var child in ChessUniformGrid.Children)
+            {
+                if (child is Border border && border.Tag is BoardCellInfo info && info.Row == row && info.Col == col)
+                    return border;
+            }
+
+            return null;
+        }
+
+        private string GetPieceResourceKey(BllColor color, BllPieceType type)
+        {
+            string prefix = color == BllColor.White ? "w" : "b";
+            string suffix = type switch
+            {
+                BllPieceType.Pawn => "P",
+                BllPieceType.Knight => "N",
+                BllPieceType.Bishop => "B",
+                BllPieceType.Rook => "R",
+                BllPieceType.Queen => "Q",
+                BllPieceType.King => "K",
+                _ => "P"
+            };
+            return $"{prefix}{suffix}";
         }
 
         private void OnGameEnded(BllColor? winner)
