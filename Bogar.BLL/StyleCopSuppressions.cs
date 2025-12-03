daa8887d6dc5460b153b77aeed059c76a46c0d36
PapproxNP <150626349+PapproxNP@users.noreply.github.com>
2025-12-03 01:35:46 +0000

    Stylecop: ignoring FieldNamesMustNotContainUnderscore

--- Bogar.BLL/StyleCopSuppressions.cs
+++ Bogar.BLL/StyleCopSuppressions.cs
@@ -1,3 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:FieldNamesMustNotBeginWithUnderscore", Justification = "Reviewed.")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:FieldNamesMustNotContainUnderscore", Justification = "Reviewed.")]
